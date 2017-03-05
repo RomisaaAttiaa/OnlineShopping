440719b6bd6ce893c50ce92c02c8fabe888cd2ef
ToqaEid <toqa.eid@gmail.com>
2017-03-05 21:20:05 +0200

    user updated, admin added

---
+++ OnlineShopping/OnlineShopping/src/main/java/com/jets/onlineshopping/controller/SessionListener.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jets.onlineshopping.controller;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author Eslam
+ */
+public class SessionListener {
+}
